706307801b7525d1d8bd17c4f7fa1911cdc68590
Robbi Chandra <robbiechandra02@gmail.com>
2023-08-22 14:13:43 +0700

    fix seeder

--- back-flopos/database/seeders/DatabaseSeeder.php
+++ back-flopos/database/seeders/DatabaseSeeder.php
@@ -19,15 +19,21 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         \App\Models\Category::factory(25)->create();
         \App\Models\Product::factory(80)->create();
-        // User::create([
-        //     'username' => 'admin',
-        //     'email' => 'admin@gmail.com',
-        //     'password' => Hash::make('admin'),
-        // ]);
-        // User::create([
-        //     'username' => 'user',
-        //     'email' => 'user@gmail.com',
-        //     'password' => Hash::make('user'),
-        // ]);
+        $user = User::where('username', 'user')->first();
+        $admin = User::where('username', 'admin')->first();
+        if (!$user) {
+            User::create([
+                'username' => 'user',
+                'email' => 'user@gmail.com',
+                'password' => Hash::make('user'),
+            ]);
+        }
+        if (!$admin) {
+            User::create([
+                'username' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin'),
+            ]);
+        }
     }
 }
